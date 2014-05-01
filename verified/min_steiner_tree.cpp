3ff1864d0c71ffe41ec8bb68aacaedcd3396e887
hadrori <hadrori.hs@gmail.com>
2014-05-01 13:32:29 +0900

    verify min_steiner_tree

--- verified/min_steiner_tree.cpp
+++ verified/min_steiner_tree.cpp
@@ -10,35 +10,35 @@
     ans:シュタイナー木の重み
 */
 
-typedef vector<int> Array;
-typedef vector<Array> Matrix;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
 
-int minimum_steiner_tree(vector<int>& T, Matrix &g){
+int minimum_steiner_tree(vi &T, vvi &g){
     int n = g.size(), t = T.size();
     if(t <= 1) return 0;
-    Matrix d(g);    // all-pair shortest
+    vvi d(g);    // all-pair shortest
 
     rep(k,n)rep(i,n)rep(j,n)    //Warshall Floyd
         d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
 
-    int OPT[1 << t][n];
+    int opt[1 << t][n];
     rep(S,1<<t) rep(x,n)
-        OPT[S][x] = INF;
+        opt[S][x] = INF;
 
     rep(p,t) rep(q,n)   // trivial case
-        OPT[1 << p][q] = d[T[p]][q];
+        opt[1 << p][q] = d[T[p]][q];
 
     repi(S,1,1<<t){   // DP step
         if(!(S & (S-1))) continue;
         rep(p,n) rep(E,S)
             if((E | S) == S)
-                OPT[S][p] = min(OPT[S][p], OPT[E][p] + OPT[S-E][p]);
+                opt[S][p] = min(opt[S][p], opt[E][p] + opt[S-E][p]);
         rep(p,n) rep(q,n)
-            OPT[S][p] = min(OPT[S][p], OPT[S][q] + d[p][q]);
+            opt[S][p] = min(opt[S][p], opt[S][q] + d[p][q]);
     }
 
     int ans = INF;
     rep(S,1<<t) rep(q,n)
-        ans = min(ans, OPT[S][q] + OPT[((1 << t) - 1) - S][q]);
+        ans = min(ans, opt[S][q] + opt[((1<<t)-1)-S][q]);
     return ans;
 }
