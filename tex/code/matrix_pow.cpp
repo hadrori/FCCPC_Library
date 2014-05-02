c07e51d24135973362aa0b252b8ae9e76962e72f
hadrori <hadrori.hs@gmail.com>
2014-05-02 13:34:44 +0900

    fix matrix_pow

--- tex/code/matrix_pow.cpp
+++ tex/code/matrix_pow.cpp
@@ -1,4 +1,4 @@
-mat pow(mat &A, int e) {
+mat pow(const mat &A, int e) {
     return e == 0 ? identity(A.size())  :
     e % 2 == 0 ? pow(mul(A, A), e/2) : mul(A, pow(A, e-1));
 }
--- verified/matrix_pow.cpp
+++ verified/matrix_pow.cpp
@@ -2,7 +2,7 @@
   累乗 O( n^3 log e )
   verified AOJ 1327
 */
-mat pow(mat &A, int e) {
+mat pow(const mat &A, int e) {
     return e == 0 ? identity(A.size())  :
     e % 2 == 0 ? pow(mul(A, A), e/2) : mul(A, pow(A, e-1));
 }
