99d089f65ce234f9e9be231b7eda7a163c30000b
stqn <stac.task@gmail.com>
2014-10-16 06:01:22 +0900

    Merge existing into 2014asia

--- 2014asia/cpp11/matrix.cpp
+++ 2014asia/cpp11/matrix.cpp
@@ -34,3 +34,41 @@ mat pow(mat A, long m) {
     } while (m >>= 1);
     return B;
 }
+
+const number eps = 1e-4;
+
+// determinant; O(n^3)
+number det(mat A) {
+    int n = A.size();
+    number D = 1;
+    rep(i,n){
+        int pivot = i;
+        repi(j,i+1,n)
+            if (abs(A[j][i]) > abs(A[pivot][i])) pivot = j;
+        swap(A[pivot], A[i]);
+        D *= A[i][i] * (i != pivot ? -1 : 1);
+        if (abs(A[i][i]) < eps) break;
+        repi(j,i+1,n)
+            for(int k=n-1;k>=i;--k)
+                A[j][k] -= A[i][k] * A[j][i] / A[i][i];
+    }
+    return D;
+}
+
+// rank; O(n^3)
+int rank(mat A) {
+    int n = A.size(), m = A[0].size(), r = 0;
+    for(int i = 0; i < m and r < n; i++){
+        int pivot = r;
+        repi(j,r+1,n)
+            if (abs(A[j][i]) > abs(A[pivot][i])) pivot = j;
+        swap(A[pivot], A[r]);
+        if (abs(A[r][i]) < eps) continue;
+        for(int k=m-1;k>=i;--k)
+            A[r][k] /= A[r][i];
+        repi(j,r+1,n) repi(k,i,m)
+            A[j][k] -= A[r][k] * A[j][i];
+        ++r;
+    }
+    return r;
+}
