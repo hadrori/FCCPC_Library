d80032465f8d3ad2919c6e62996c5ad7a4b3792d
hadrori <hadrori.hs@gmail.com>
2014-10-16 11:55:28 +0900

    Update tex code

--- tex/code/mod_pow.cpp
+++ tex/code/mod_pow.cpp
@@ -1,5 +1,5 @@
-ll pow_mod(ll x, ll k, ll m) {
-    if (k == 0)     return 1;
-    if (k % 2 == 0) return pow_mod(x*x % m, k/2, m);
-    else            return x*pow_mod(x, k-1, m) % m;
+int pow_mod(int x, int k, int m) {
+    int ret = 1;
+    for(x%=m; k>0; x=1LL*x*x%m,k>>=1) if(k&1) ret = 1LL*ret*x%m;
+    return ret;
 }
