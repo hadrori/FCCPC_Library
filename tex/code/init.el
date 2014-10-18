2a288952b471fcfab20e7bc0f0674b3c535d1322
hadrori <hadrori.hs@gmail.com>
2014-10-19 00:16:08 +0900

    Add Wavelet

--- tex/code/init.el
+++ tex/code/init.el
@@ -1,13 +1,4 @@
-;key
 (keyboard-translate ?\C-h ?\C-?)
-(global-set-key "\M-g" 'goto-line)
-
-;tab
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
-
-;line number
 (global-linum-mode t)
 (setq linum-format "%4d ")
 
