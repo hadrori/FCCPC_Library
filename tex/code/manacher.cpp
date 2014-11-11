b61f241f726b241c8d691de8ab0f1c8c3f6d43d2
hadrori <hadrori.hs@gmail.com>
2014-11-11 20:57:17 +0900

    Add manacher

---
+++ tex/code/manacher.cpp
@@ -0,0 +1,11 @@
+vector<int> manacher(const string &s) {
+    int n = s.size()*2;
+    vector<int> rad.assign(n,0);
+    for (int i = 0, j = 0, k; i < n; i += k, j = max(j-k, 0)) {
+        while (i-j >= 0 && i+j+1 < n && s[(i-j)/2] == s[(i+j+1)/2]) ++j;
+        rad[i] = j;
+        for (k = 1; i-k >= 0 && rad[i]-k >= 0 && rad[i-k] != rad[i]-k; ++k)
+            rad[i+k] = min(rad[i-k], rad[i]-k);
+    }
+    return rad;
+}
