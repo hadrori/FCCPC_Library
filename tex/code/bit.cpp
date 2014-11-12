5f88e9f3e469efdb0343116cef46a2be8a62fe6e
hadrori <hadrori.hs@gmail.com>
2014-11-12 23:24:40 +0900

    Add some data structure

---
+++ tex/code/bit.cpp
@@ -0,0 +1,19 @@
+template<class T> struct bit
+{
+    int n;
+    vector<T> dat;
+
+    bit(int n) : n(n){
+        dat.assign(n,0);
+    }
+    // sum [0,i)
+    T sum(int i){
+        int ret = 0;
+        for(--i; i>=0; i=(i&(i+1))-1) ret += bit[i];
+        return ret;
+    }
+    // sum [i,j)
+    T sum(int i, int j){ return sum(j) - sum(i);}
+    // add x to i
+    void add(int i, T x){ for(; i < n; i|=i+1) bit[i] += x;}
+};
