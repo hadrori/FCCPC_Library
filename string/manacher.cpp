bdf228e1bafe78f521d2379ee24d5c3c5275dbb5
hadrori <hadrori.hs@gmail.com>
2015-11-10 14:20:32 +0900

    Fix manacher

--- string/manacher.cpp
+++ string/manacher.cpp
@@ -1,6 +1,6 @@
 vector<int> manacher(const string &s) {
     int n = s.size()*2;
-    vector<int> rad.assign(n,0);
+    vector<int> rad(n,0);
     for (int i = 0, j = 0, k; i < n; i += k, j = max(j-k, 0)) {
         while (i-j >= 0 && i+j+1 < n && s[(i-j)/2] == s[(i+j+1)/2]) ++j;
         rad[i] = j;
