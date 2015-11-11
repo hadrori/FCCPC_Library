614e11574147de3c9c01b06ae60b0272462dfe04
stqn <stac.task@gmail.com>
2015-11-11 18:19:22 +0900

    Modify Z-algorithm

--- string/lcp0.cpp
+++ string/lcp0.cpp
@@ -2,13 +2,13 @@ vector<int> lcp0(const string& s) {
     const int n = s.length();
     vector<int> ret(n);
     ret[0] = n;
-    int i = 1, j = 0;
-    while (i < n) {
+    for (int i = 1, j = 0, k; i < n; ) {
         while (i+j < n and s[i+j] == s[j]) ++j;
         ret[i] = j;
         if (j == 0) { ++i; continue; }
-        int k = 1;
-        while (i+k < n and k+ret[k] < j) ret[i+k] = ret[k], ++k;
+        for (k = 1; i+k < n and k+ret[k] < j; ++k) {
+            ret[i+k] = ret[k];
+        }
         i += k, j -= k;
     }
     return ret;
